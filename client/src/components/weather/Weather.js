f6fa0eb6e652c3d3df0e9aa3309fe48e42a1c597
unknown <amehi4yus@gmail.com>
2021-12-25 11:06:02 -0800

    Temperature degree

--- client/src/components/weather/Weather.js
+++ client/src/components/weather/Weather.js
@@ -7,22 +7,12 @@ const Weather = ({ showForecast, setShowForecast }) => {
 
     const [currentWeather] = useOpenWeatherAPI()
 
-    /*const [temperature, setTemperature] = useState(currentWeather.temp)
-    const [isCelcius, setIsCelcius] = useState(false)
-
-        let celcius = (temperature - 32) / 1.8 
-        if(isCelcius)setTemperature(celcius)
-        <h2 className="temperature-degree" onClick={()=>setIsCelcius(!isCelcius)}>{Math.round(temperature)}&#176;</h2>
-        
-        */
-    
-
     return (
         <div className="temperature-container">
             <div className="temperature">
                 <div className="temperature-section">
                     <i className={`wi wi-owm-${currentWeather.id}`}></i>
-                    <h2 className="temperature-degree">{Math.round(currentWeather.temp)}</h2>
+                    <h2 className="temperature-degree">{Math.round(currentWeather.temp)} &#176;</h2>
                     <span></span>
                 </div>
                 <h1 className="temperature-city" onClick={()=>setShowForecast(!showForecast)}>{currentWeather.cityName}</h1>
