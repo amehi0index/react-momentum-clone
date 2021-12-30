3268d9ebcbd69cd0a15aa36dddaf1fa6ed9b60f5
unknown <amehi4yus@gmail.com>
2021-12-29 16:43:01 -0800

    Added preloader

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,40 @@
 
 
+import React, { useState, useEffect } from 'react'
+import Loader from "react-loader-spinner"
 import ContainerOverlay from './components/ContainerOverlay'
 import BackgroundImage from './components/BackgroundImage'
 import useTodoShow from './hooks/useTodoShow'
+import useUnsplashAPI from './hooks/useUnsplashAPI'
 
 const App = () => {
   const [showTodoCard, setShowTodoCard] = useTodoShow()
+  const [ imageData ] = useUnsplashAPI()
+  const [ loading, setLoading ] = useState(true)
+ 
+  useEffect(() => {
+    if (imageData.loading === false){
+      setLoading(false)
+    } 
+  }, [imageData.loading])
 
   return (
     <>
-      <BackgroundImage/>
-      <ContainerOverlay showTodoCard={showTodoCard} setShowTodoCard={setShowTodoCard} />
+      {loading 
+        ?  <div className="loader-container">
+            <Loader
+              className="loader"
+              type="ThreeDots"
+              color="#ffff"
+              height={150}
+              width={150}
+            /> 
+          </div>
+        : <>
+            <BackgroundImage/>
+            <ContainerOverlay showTodoCard={showTodoCard} setShowTodoCard={setShowTodoCard} />
+          </>
+      }
     </>
   )
 }
--- client/src/components/ContainerOverlay.js
+++ client/src/components/ContainerOverlay.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react'
-import Loader from "react-loader-spinner";
 import useUnsplashAPI from '../hooks/useUnsplashAPI'
 import Greeting from '../components/greeting/Greeting'
 import SearchForm from '../components/search/SearchForm'
@@ -16,53 +15,30 @@ const ContainerOverlay = ({ showTodoCard, setShowTodoCard }) => {
 
     const [ imageData ] = useUnsplashAPI()
     const [ showForecast, setShowForecast ] = useState(false)
-    const [ loading, setLoading ] = useState(true)
-
-    useEffect(() => {
-        if (imageData.loading === false){
-            setLoading(false)
-        }  
-    },[imageData])
-
 
     let clr = tinycolor(imageData.color)
     let isColorLight =  clr.isLight() 
 
     return (
-     
         <div className= {isColorLight ? 'container bg-darken' : 'container'}>
-
-            {loading 
-                ?  <Loader
-                    className="loader"
-                    type="ThreeDots"
-                    color="#00000"
-                    height={100}
-                    width={100}
-                    /> 
-                    
-                :  <>
-                        <div className="top-grid">
-                            <div className="one">
-                                <SearchForm />
-                            </div>
-                            <div className="two">
-                                <Greeting />
-                            </div>
-                            <div className="three">
-                                <Weather showForecast={showForecast} setShowForecast={setShowForecast}/>
-                                <Forecast showForecast={showForecast}/>
-                                <Todos showTodoCard={showTodoCard} />
-                            </div>
-                        </div>
-                        <div className= "four">
-                            <ImageLocation />
-                            <Quote />
-                            <TodoToggle showTodoCard={showTodoCard}  setShowTodoCard={setShowTodoCard} />
-                        </div>
-                    </>
-                }
-                       
+            <div className="top-grid">
+                <div className="one">
+                    <SearchForm />
+                </div>
+                <div className="two">
+                    <Greeting />
+                </div>
+                <div className="three">
+                    <Weather showForecast={showForecast} setShowForecast={setShowForecast}/>
+                    <Forecast showForecast={showForecast}/>
+                    <Todos showTodoCard={showTodoCard} />
+                </div>
+            </div>
+            <div className= "four">
+                <ImageLocation />
+                <Quote />
+                <TodoToggle showTodoCard={showTodoCard}  setShowTodoCard={setShowTodoCard} />
+            </div>                  
         </div>
     )
 }
